d8436aa50c380c49c558888e82196ea7d1a49dee
wnstjd9701 <wnstjd9701@naver.com>
2021-07-10 17:50:50 +0900

    Stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -2,7 +2,7 @@
 from collections import deque
 stack = deque()
 
-# 스택 맨 끝에 데이터 추가
+# 스택 맨 끝에 데이터 추가 - append
 stack.append("T")
 stack.append("a")
 stack.append("e")
@@ -13,7 +13,7 @@ print(stack)
 
 print(stack[-1])
 
-# 맨 끝 데이터 삭제
+# 맨 끝 데이터 삭제 - pop function
 print(stack.pop())
 print(stack.pop())
 print(stack.pop())
